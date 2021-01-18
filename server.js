7b7da6607b1dc4f050fcebbd6ea0f4748255d24f
rey <rhernandez@orfis.gob.mx>
2021-01-18 13:14:46 -0600

    addint the rest api end points for user accounts

--- server.js
+++ server.js
@@ -10,9 +10,22 @@ app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 
-app.get('/',(req,res)=>{
-    res.json({messsage:'Welcome to abcdatoz app'})
-})
+
+const db = require("./app/models");
+const Role = db.role;
+ 
+db.sequelize.sync({force:true})
+    .then(()=>{
+        console.log("Drop and Resync Db");        
+        initial()
+    });
+
+
+app.get('/',(req,res)=>{ res.json({messsage:'Welcome to abcdatoz app'}) })
+
+require('./app/routes/authRoutes')(app);
+require('./app/routes/userRoutes')(app);
+
 
 const PORT = process.env.PORT || 8080
 
@@ -24,3 +37,20 @@ app.listen(PORT, ()=>{
 
 
 
+function initial(){
+    Role.create({
+        id:1,
+        name:"user"
+    });
+
+    Role.create({
+        id:2,
+        name:"moderator"
+    });
+
+    Role.create({
+        id:3,
+        name:"admin"
+    })
+
+}
